a5dc9f9ef0342abe059e30db5bdfa630819e7944
Lucasrusso36 <lucaspidossantos@gmail.com>
2023-04-11 20:15:20 -0300

    feature: create cities

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_230852) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_11_231102) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.bigint "state_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["state_id"], name: "index_cities_on_state_id"
+  end
 
   create_table "states", force: :cascade do |t|
     t.string "name"
@@ -20,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_230852) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cities", "states"
 end
