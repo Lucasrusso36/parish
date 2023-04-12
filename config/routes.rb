8c07c4c7310abc3d4aa537aafebcb6c6a1430429
Lucasrusso36 <lucaspidossantos@gmail.com>
2023-04-11 22:21:17 -0300

    feature: add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
-  resources :additional_informations
-  resources :faithfuls
-  resources :relationships
-  resources :communities
+  devise_for :admins
+
+  authenticate :admin do
+  # resources :additional_informations
+  # resources :faithfuls
+  # resources :relationships
+  # resources :communities
   # resources :sexual_orientations
   # resources :streets
   # resources :neighborhoods
   # resources :cities
   # resources :states
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
