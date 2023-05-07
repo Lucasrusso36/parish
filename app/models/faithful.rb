d40730b91a35b3191599de299f06e7122540372a
Lucasrusso36 <lucaspidossantos@gmail.com>
2023-05-07 00:15:26 -0300

    feature: create in seed csv for import faithful

--- app/models/faithful.rb
+++ app/models/faithful.rb
@@ -11,6 +11,6 @@ class Faithful < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :phone_number, presence: true
+  #validates :phone_number, presence: true
   #validates :date_of_birth, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# CSV FOR IMPORT FAITHFUL
+# require 'csv'
+
+# CSV.foreach('public/ecc.csv', headers: true) do |row|
+#   name_parts = row['nome'].split(' ')
+#   first_name = name_parts.shift
+#   last_name = name_parts.join(' ')
+  
+#   faithful = Faithful.create!(
+#     first_name: first_name,
+#     last_name: last_name,
+#     function: row['função'],
+#     community_id: row['comunidade'],
+#     phone_number: row['telefone'],
+#     date_of_birth: row['data_de_nascimento'],
+#     sexual_orientation: SexualOrientation.find_by(id: row['orientação_sexual'].to_i),
+#     relationship_id: row['relacionamento'],
+#     wedding_date: row['data_de_casamento'],
+#     children: row['filhos'],
+#     state_id: row['estado'],
+#     city_id: row['cidade'],
+#     neighborhood_id: row['bairro'],
+#     street_id: row['rua'],
+#     house_number: row['numero_da_casa'],
+#     complement: row['complemento']
+#   )
+# end
